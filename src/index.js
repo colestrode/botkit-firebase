980e5872db12d1a3dd40c8d7ae47e3d92285ff66
Cole Furfaro-Strode <colestrode@gmail.com>
2016-01-06 21:34:09 -0500

    Clean up the firebase lib

--- src/index.js
+++ src/index.js
@@ -1,83 +1,23 @@
-/*
- Firebase storage module for bots.
-
- Note that this storage module does not specify how to authenticate to Firebase.
- There are many methods of user authentication for Firebase.
- Please read: https://www.firebase.com/docs/web/guide/user-auth.html
-
- Supports storage of data on a team-by-team, user-by-user, and chnnel-by-channel basis.
-
- save can be used to store arbitrary object.
- These objects must include an id by which they can be looked up.
- It is recommended to use the team/user/channel id for this purpose.
- Example usage of save:
- controller.storage.teams.save({id: message.team, foo:"bar"}, function(err){
- if (err)
- console.log(err)`
- });
-
- get looks up an object by id.
- Example usage of get:
- controller.storage.teams.get(message.team, function(err, team_data){
- if (err)
- console.log(err)
- else
- console.log(team_data)
- });
- */
-
 var Firebase = require('firebase');
 
+/**
+ * The Botkit firebase driver
+ *
+ * @param {Object} config This must contain a `firebase_uri` property
+ * @returns {{teams: {get, save, all}, channels: {get, save, all}, users: {get, save, all}}}
+ */
 module.exports = function(config) {
 
-    if (!config && !config.firebase_uri)
-        throw new Error('Need to provide firebase address. This should look something like ' +
-            '"https://botkit-example.firebaseio.com/"');
+    if (!config || !config.firebase_uri)
+        throw new Error('Please provide a firebase address. ' +
+            'This should look something like "https://botkit-example.firebaseio.com/"');
 
-    var rootRef = new Firebase(config.firebase_uri);
-    var teamsRef = rootRef.child('teams');
-    var usersRef = rootRef.child('users');
-    var channelsRef = rootRef.child('channels');
+    var rootRef = new Firebase(config.firebase_uri),
+        teamsRef = rootRef.child('teams'),
+        usersRef = rootRef.child('users'),
+        channelsRef = rootRef.child('channels');
 
-    var get = function(firebaseRef) {
-        return function(id, cb) {
-            firebaseRef.child(id).once('value',
-                function(records) {
-                    cb(undefined, records.val());
-                },
-                function(err) {
-                    cb(err, undefined);
-                }
-            );
-        };
-    };
-
-    var save = function(firebaseRef) {
-        return function(data, cb) {
-            var firebase_update = {};
-            firebase_update[data.id] = data;
-            firebaseRef.update(firebase_update, cb);
-        };
-    };
-
-    var all = function(firebaseRef) {
-        return function(cb) {
-            firebaseRef.once('value',
-                function(records) {
-                    var list = [];
-                    for (key in Object.keys(records.val())) {
-                        list.push(records.val()[key]);
-                    }
-                    cb(undefined, list);
-                },
-                function(err) {
-                    cb(err, undefined);
-                }
-            );
-        };
-    };
-
-    var storage = {
+    return {
         teams: {
             get: get(teamsRef),
             save: save(teamsRef),
@@ -94,7 +34,55 @@ module.exports = function(config) {
             all: all(usersRef)
         }
     };
-
-    return storage;
-
 };
+
+/**
+ * Given a firebase ref, will return a function that will get a single value by ID
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The get function
+ */
+function get(firebaseRef) {
+    return function(id, cb) {
+        firebaseRef.child(id).once('value', success, cb);
+
+        function success(records) {
+            cb(null, records.val());
+        }
+    };
+}
+
+/**
+ * Given a firebase ref, will return a function that will save an object. The object must have an id property
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The save function
+ */
+function save(firebaseRef) {
+    return function(data, cb) {
+        var firebase_update = {};
+        firebase_update[data.id] = data;
+        firebaseRef.update(firebase_update, cb);
+    };
+}
+
+/**
+ * Given a firebase ref, will return a function that will return all objects stored.
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The all function
+ */
+function all(firebaseRef) {
+    return function(cb) {
+        firebaseRef.once('value', success, cb);
+
+        function success(records) {
+            var list = [],
+                key;
+            for (key in Object.keys(records.val())) {
+                list.push(records.val()[key]);
+            }
+            cb(undefined, list);
+        }
+    };
+}
